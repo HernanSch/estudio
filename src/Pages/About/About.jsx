f6cadb9086d0e1c7299de41089bd5a4aa9414c82
Hernan <hschiavonr@gmail.com>
2023-01-19 20:44:40 +0100

    formik emailjs up

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Form3 from '../../Components/form3/form3'
+import FormikEmailjs from '../../Components/FormikEmailjs/FormikEmailjs'
 import './About.scss'
 
 
 const About = () => {
   return (
     <div className='about'>
-      <Form3></Form3>
+      <FormikEmailjs></FormikEmailjs>
     </div>
   )
 }
